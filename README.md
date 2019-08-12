83b3d29a82062d5caf00cdbabddfee23d50e8443
SaadMTSA <saadmtsa@gmail.com>
2019-08-11 22:38:43 -0400

    bunch of changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # packets_learner
+Packet learner is a network attacks predictor. It is based on the **CTU-13** dataset.
--- src/detector.py
+++ src/detector.py
@@ -4,9 +4,9 @@ from functools import partial
 from data.pcap_processor import process_pcap_to_csv
 from data.netflow_aggregator import write_netflow_agg_scenario
 from data.pcap_aggregator import write_pcap_agg_scenario
-from train import train_models
-from train_nn import train_rnn_models
-from compare_evaluations import compare_evaluations
+from commands.train import train_models
+from commands.train_nn import train_rnn_models
+from commands.compare_evaluations import compare_evaluations
 
 click.option = partial(click.option, show_default=True)
 
